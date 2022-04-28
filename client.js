2847d8d5baf065551f21cd5d5ffab5093820f58e
Daniel Pires Quirino <danielpiresquirino@RJ-MB4559.local>
2022-04-28 17:35:00 -0300

    contentful poc

---
+++ client.js
@@ -0,0 +1,6 @@
+import * as contentul from 'contentful';
+
+export const client = contentul.createClient({
+    space:'x0n4ijna36pu',
+    accessToken:'GzyO2SoCbHceNx9cnep0QvAYqqeov0uDHFt26FQX4xc'
+})
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,19 @@
 import Head from 'next/head'
+import { useEffect, useState } from 'react'
+import { client } from '../client';
 
 export default function Home() {
+
+  const [imoveis, setImoveis] = useState([])
+
+  useEffect(() => {
+    client.getEntries()
+      .then((response) => {
+        setImoveis(response.items)
+      })
+  },[])
+
+
   return (
     <div className="container">
       <Head>
@@ -9,43 +22,22 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        {console.log(imoveis)}
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        {
+          imoveis.map((imovel) => {
+            return (
+              <div>
+              <div> {imovel.fields.title}</div>
+              <img src={imovel.fields.image.fields.file.url} />
+              <div> {imovel.fields.quartos} </div>
+              <br/><br/>
+              </div>
+            )
+          })
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        }
+        
       </main>
 
       <footer>
